7f9dd9517426bed0099cf627d79fe4c5787ff0b7
HosseinAfsharnia <h.afsharnia@gmail.com>
2021-12-22 06:53:54 -0800

    Change in the Stock and Client and Add the Order class

--- ApplicationStock/ConsoleStockBruker/ProjectStockLibrary/Order.cs
+++ ApplicationStock/ConsoleStockBruker/ProjectStockLibrary/Order.cs
@@ -6,8 +6,55 @@ using System.Threading.Tasks;
 
 namespace ProjectStockLibrary
 {
-    public class Order
+    internal class Order
     {
-        
+        private Guid _id;
+        private string _orderName { get; set; }
+        private DateTime _orderDate;
+        private Stock _stock { get; set; }
+        private  int _nbStock { get; set; }
+        public Order(string orderName,Stock stock, int nbStock) 
+        {
+            _orderName = orderName;
+            _orderDate = DateTime.Now;
+            _id = Guid.NewGuid();
+            _stock = stock;
+            _nbStock = nbStock;
+
+        }
+        public Order(string orderName,Stock stock, int nbStock, DateTime orderDate, Guid id ) : this(orderName, stock, nbStock)
+        {
+            _orderName = string.IsNullOrEmpty(orderName) ? throw new ArgumentNullException(nameof(orderName)) : orderName;
+        }
+        public void AddStocks(Stock stock)
+        {
+            if (!(stock is null))
+            {
+                _stock = stock;
+              
+            }
+            else
+            {
+                throw new Exception("The stock is not Defined");
+            }
+
+        }
+
+        public void modifyStock(Stock stock,string newNameStock, string newNameEnterprise, int newnbStock)
+        {
+            if (_stock == stock)
+            {
+                _stock._name = newNameStock;
+                _stock._entrepriseName = newNameEnterprise;
+                this._nbStock = newnbStock;
+
+            }
+            else
+            {
+                throw new Exception("The Stock Does not correspond to the Chosen One! Please Check!");
+            }
+
+        }
     }
+    
 }
