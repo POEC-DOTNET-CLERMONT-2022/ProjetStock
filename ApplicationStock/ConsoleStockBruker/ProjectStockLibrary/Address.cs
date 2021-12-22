7f9dac6244f2dc4d169d44695404ead3fe09dc25
HosseinAfsharnia <h.afsharnia@gmail.com>
2021-12-22 01:00:27 -0800

    Initiate Project src codes

---
+++ ApplicationStock/ConsoleStockBruker/ProjectStockLibrary/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectStockLibrary
+{
+    internal class Address
+    {
+        private Guid _id;
+        private string _address_line_1;
+        private string _address_line_2;
+        private string _codePostal;
+        private string _city;
+        private string _country;
+        public Address(string address_line_1, string address_line_2, string codePostal, string city, string country)
+        {
+             _id = Guid.NewGuid();
+            _address_line_1 = address_line_1;
+            _address_line_2 = address_line_2;
+            _codePostal = codePostal;
+            _city = city;
+            _country = country;
+        }
+         
+    }
+    
+}
---
+++ ApplicationStock/ConsoleStockBruker/ProjectStockLibrary/Client.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjectStockLibrary
+{
+     internal class Client
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _phone;
+        private string _siret;
+        private Address _address;
+
+        public Client(string firstName , string lastName, string email, string phone, string siret, Address address)
+        {
+            _id = Guid.NewGuid();
+            _firstName = firstName;
+            _lastName = lastName;                  
+            _email = email;
+            _siret = siret;
+            _phone = phone;
+            _address = address;
+
+
+        }
+        public Client(Guid id, string firstName , string lastName, string email, string phone, string siret, Address address) : this(firstName, lastName, email, phone, siret, address)
+        {
+            _id = id;
+        }
+
+    }
+}
