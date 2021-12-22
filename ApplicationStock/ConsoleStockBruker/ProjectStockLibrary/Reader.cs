a43c1812ac2b743aa79344432b8545102d9af408
tristanblc <tblanc.sio@gmail.com>
2021-12-22 21:52:15 +0100

    changes on reader.cs

--- ApplicationStock/ConsoleStockBruker/ProjectStockLibrary/Reader.cs
+++ ApplicationStock/ConsoleStockBruker/ProjectStockLibrary/Reader.cs
@@ -7,29 +7,69 @@ using MyStockModels;
 
 namespace ProjectStockLibrary
 {
-    public class Reader<TItem> : IReader<TItem>
+    public class Reader<TItem> :IReader<TItem>
     {
         public IWriter<TItem> Writer { get; set; }
+
+
+
         public void read()
         {
 
-
-
-
         }
-        public TItem Read()
+
+        public TItem create() 
         {
-            Writer.Display("Donne moi le nom d'un ingredient");
+
+            Writer.Display("Creer un objet");
+            TItem item = Activator.CreateInstance<TItem>();
+            Writer.Display("Quel nom ?");
             var name = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(ingredientName))
+            if (string.IsNullOrWhiteSpace(name))
             {
-                Writer.Display("Ce nom n'est pas bon !");
-                return read
+                Writer.Display("Ré-écrire le nom");
+                this.read();
             }
+            if(item is Stock )
+            {
+                Writer.Display("Quel valeur ?");
+                string valeur  = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(valeur))
+                {
+                    Writer.Display("Ré-écrire la valeur ");
+                    this.read();
+                }
+                float valeur_ = float.Parse(valeur);
+                Writer.Display("Quel entreprise?");
+                var nameEntreprise = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(nameEntreprise))
+                {
+                    Writer.Display("Ré-écrire le nom de l'entreprise");
+                    this.read();
+                }
+            }
+            if(item is Order)
+            {
+                Writer.Display("Quel nbstock ?");
+                string valeur = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(valeur))
+                {
+                    Writer.Display("Ré-écrire le nbstock ");
+                    this.read();
+                }
+                float valeur_ = float.Parse(valeur);
+                Writer.Display("Quel ordername?");
+                var nameEntreprise = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(nameEntreprise))
+                {
+                    Writer.Display("Ré-écrire le nom de l'ordernaem");
+                    this.read();
+                }
+            }
+            
 
-            return new TItem { }
-          }
+            return item;
+        }
     
     }
 }
