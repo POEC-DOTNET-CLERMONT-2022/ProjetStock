00d4fdf7b5573c53ddb2a83afaf4aec7f8464741
tristanblc <tblanc.sio@gmail.com>
2021-12-22 16:29:07 +0100

    change program.cs

--- ApplicationStock/ConsoleStockBruker/ConsoleStockBruker/Program.cs
+++ ApplicationStock/ConsoleStockBruker/ConsoleStockBruker/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using MyStockModels;
+using ProjectStockLibrary;
+
+
 Console.WriteLine("Hello, World!");
+
+Manager<Market> marketManager = new Manager<Market>(new Writer<Market>(), new Reader<Market>());
+
+
+
+while (true)
+{
+    Console.WriteLine("1: read, 2: create");
+    var choice = int.Parse(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            marketManager.read();
+            break;
+        case 2:
+            marketManager.Create();
+            break;
+        default:
+            Console.WriteLine("Fin du programme");
+
+            Environment.Exit(0);
+            break;
+    }
+
+}
+
+
+
+
+Console.ReadLine();
