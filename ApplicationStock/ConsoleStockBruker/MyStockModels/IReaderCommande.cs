932644432d6c09115509d8d59854f50b89f9617b
tristanblc <tblanc.sio@gmail.com>
2021-12-30 17:50:32 +0100

    Changes and a add persistance project

---
+++ ApplicationStock/ConsoleStockBruker/MyStockModels/IReaderCommande.cs
@@ -0,0 +1,16 @@
+﻿using ProjectStockLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyStockModels
+{
+    public interface IReaderCommande
+    {
+        public IWriterCommande Writer { get; set; }
+        public Order create(Stock stock);
+    }
+}
---
+++ ApplicationStock/ConsoleStockBruker/PersistanceStockProjectLibrary/JsonSerializer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PersistanceStockProjectLibrary
+{
+    public class JsonSerializer
+    {
+    }
+}
