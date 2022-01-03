45762820be91c46e8469c18f62ae61efdba3631d
tristanblc <tblanc.sio@gmail.com>
2022-01-03 16:34:46 +0100

    Changes  and add Wpf

---
+++ ApplicationStock/ConsoleStockBruker/WPF_Application/ListOrderControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ProjectStockLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WPF_Application
+{
+    /// <summary>
+    /// Logique d'interaction pour ListOrderControl.xaml
+    /// </summary>
+    public partial class ListOrderControl : UserControl
+    {
+        
+        public ListOrderControl()
+        {
+            //List<Order> _orders = new List<Order>();
+            //Stock stock = new Stock("etst", 5, "tests");
+            //_orders.Add(new Order("testc",stock , 5));
+
+
+            //listsOrders.ItemsSource = _items;
+
+
+            InitializeComponent();
+        }
+
+        private void ChangeViewOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ApplicationStock/ConsoleStockBruker/WPF_Application/LoginControle.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Application
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginControle.xaml
+    /// </summary>
+    public partial class LoginControle : UserControl
+    {
+        public LoginControle()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_Recover(object sender, RoutedEventArgs e)
+        {
+            SendMailReceoverPassword app_recover = new SendMailReceoverPassword();
+            userMenu.Content = app_recover;
+
+        }
+
+        private void MenuItem_Click_Create(object sender, RoutedEventArgs e)
+        {
+            CreateUser app_user = new CreateUser();
+            userMenu.Content = app_user;
+        }
+    }
+}
