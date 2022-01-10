efe4052ed287abe720457d3eb7b409b2be1ff659
tristanblc <tblanc.sio@gmail.com>
2022-01-10 20:53:36 +0100

    Add changes and add reference (service)

--- ApplicationStock/ConsoleStockBruker/WCF_application/ServiceMarche.svc.cs
+++ ApplicationStock/ConsoleStockBruker/WCF_application/ServiceMarche.svc.cs
@@ -4,35 +4,24 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using PersistanceStockProjectLibrary;
 using PersistanceStockProjectLibrary.Interfaces;
 using PersistanceStockProjectLibrary.Manager;
 using ProjectStockDTOS;
 using ProjectStockLibrary;
 using ProjectStockPatternsLibrary;
+
 namespace WCF_application
 {
-    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ServiceMarket" à la fois dans le code, le fichier svc et le fichier de configuration.
-    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ServiceMarket.svc ou ServiceMarket.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
-    public class ServiceMarket : IServiceMarket
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ServiceMarche" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ServiceMarche.svc ou ServiceMarche.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ServiceMarche : IServiceMarche
     {
-       private readonly IMarketManager _marketManager;
-
-        public ServiceMarket()
+        private readonly IMarketManager _marketManager;
+        public ServiceMarche()
         {
             _marketManager = new MarketManager();
         }
-
-
-        public IEnumerable<Market> GetAllMarkets()
-        {
-            return _marketManager.GetAllMarkets();
-
-        }
-
-        /// <summary>
-        /// Gets codex creatures
-        /// </summary>
-        /// <returns></returns>
         public IEnumerable<MarketDto> GetMarkets()
         {
             return _marketManager.GetMarkets();
