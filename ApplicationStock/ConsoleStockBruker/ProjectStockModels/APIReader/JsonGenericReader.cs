48a0257ca2c445164066051007fb3e13a5f1a238
tristanblc <tblanc.sio@gmail.com>
2022-01-18 15:13:38 +0100

    changes JsonGenericReader.cs

--- ApplicationStock/ConsoleStockBruker/ProjectStockModels/APIReader/JsonGenericReader.cs
+++ ApplicationStock/ConsoleStockBruker/ProjectStockModels/APIReader/JsonGenericReader.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -23,7 +25,7 @@ namespace WPF_Application.JsonReader
             _mapper = mapper;
             _httpClient = new HttpClient();
             // Update port # in the following line.
-            _httpClient.BaseAddress = new Uri("http://localhost:7136/api/Address/");
+            _httpClient.BaseAddress = new Uri("http://localhost:7136/api/"+typeof(T).ToString());
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIzNDY3Yjk5LTBmM2UtNDJkZi1hN2FjLTQzODY5YTFlMDdjMCIsIm5iZiI6MTY0MjQwNDY3MSwiZXhwIjoxNjQzMDA5NDcxLCJpYXQiOjE2NDI0MDQ2NzF9.3XvBwc9RVmZyVUGvaZqkAQX6Hh4Yn69uEhVdzFo-nAw");
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(
@@ -33,17 +35,23 @@ namespace WPF_Application.JsonReader
         public async Task<T> Get(T item)
         {
 
-            var request = new HttpRequestMessage(HttpMethod.Get, _httpClient.BaseAddress);
-            request.Headers.Add("Accept", "application/json");
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = _httpClient.BaseAddress,
+                Content = new StringContent(JsonConvert.SerializeObject(item))
+            };
+            httpRequestMessage.Headers.Add("Accept", "application/json");
+           
 
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.SendAsync(httpRequestMessage);
 
 
             if (response.IsSuccessStatusCode)
             {
 
                 using var responseStream = await response.Content.ReadAsStreamAsync();
-                var res = await JsonSerializer.DeserializeAsync<T>(responseStream);
+                var res = await System.Text.Json.JsonSerializer.DeserializeAsync<T>(responseStream);
 
                 return item;
             }
@@ -55,17 +63,22 @@ namespace WPF_Application.JsonReader
         public async Task<T> Update(T item)
         {
 
-            var request = new HttpRequestMessage(HttpMethod.Put, _httpClient.BaseAddress);
-            request.Headers.Add("Accept", "application/json");
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Put,
+                RequestUri = _httpClient.BaseAddress,
+                Content = new StringContent(JsonConvert.SerializeObject(item))
+            };
+            httpRequestMessage.Headers.Add("Accept", "application/json");
 
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.SendAsync(httpRequestMessage);
 
 
             if (response.IsSuccessStatusCode)
             {
 
                 using var responseStream = await response.Content.ReadAsStreamAsync();
-                var res = await JsonSerializer.DeserializeAsync<T>(responseStream);
+                var res = await System.Text.Json.JsonSerializer.DeserializeAsync<T>(responseStream);
 
 
             }
@@ -76,11 +89,16 @@ namespace WPF_Application.JsonReader
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<int> Delete(T item)
         {
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = _httpClient.BaseAddress,
+                Content = new StringContent(JsonConvert.SerializeObject(item))
+            };
 
-            var request = new HttpRequestMessage(HttpMethod.Delete, _httpClient.BaseAddress);
-            request.Headers.Add("Accept", "application/json");
+            httpRequestMessage.Headers.Add("Accept", "application/json");
 
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.SendAsync(httpRequestMessage);
 
 
 
@@ -99,10 +117,15 @@ namespace WPF_Application.JsonReader
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<int> Add(T item)
         {
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = _httpClient.BaseAddress,
+                Content = new StringContent(JsonConvert.SerializeObject(item))
+            };
+            httpRequestMessage.Headers.Add("Accept", "application/json");
 
-            var request = new HttpRequestMessage(HttpMethod.Post, _httpClient.BaseAddress);
-
-            var response = await _httpClient.SendAsync(request);
+            var response = await _httpClient.SendAsync(httpRequestMessage);
 
 
 
