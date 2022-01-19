c1a1a24a7a844af818a0f621c30c7c70c90ae95e
hafsharn <hossein.afsharnia@cern.ch>
2022-01-19 13:22:13 +0100

    Added the Crypto and its API features

---
+++ ApplicationStock/ConsoleStockBruker/ProjectStockPatternsLibrary/Factory/CryptoFactory.cs
@@ -0,0 +1,28 @@
+using ProjectStockDTOS;
+using ProjectStockLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectStockPatternsLibrary
+
+{ 
+
+    public static class CryptoFactory
+    {
+        public static IEnumerable<Crypto> ToDto(this IEnumerable<CryptoDto> cryptos)
+        {
+            foreach (var crypto in cryptos)
+            {
+                yield return crypto.ToModel();
+            }
+                      
+        }
+
+        public static Crypto ToModel(this CryptoDto crypto)
+        {
+            return new Crypto(crypto._id) { _name = crypto._name, _value = crypto._value };
+    }
+    }
+}
