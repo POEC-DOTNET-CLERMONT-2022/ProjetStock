3f9f0d00d15fc975fc5c932ae41c2afa84044b0e
HosseinAfsharnia <h.afsharnia@gmail.com>
2022-01-20 01:14:45 -0800

    Adds the Crypto part to ProjectstockModels

---
+++ ApplicationStock/ConsoleStockBruker/ProjectStockModels/Lists/CryptoLists.cs
@@ -0,0 +1,49 @@
+﻿using ProjectStockModels.Model;
+using ProjectStockModels.Observable;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectStockModels.Lists
+{
+    public class CryptoLists : ObservableObject
+    {
+        private ObservableCollection<CryptoModel> _Cryptos;
+
+        private CryptoModel _crypto;
+
+
+        public CryptoModel currentCrypto
+        {
+            get { return currentCrypto; }
+            set
+            {
+
+                if (currentCrypto != value)
+                {
+                    currentCrypto = value;
+                    OnNotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        public ObservableCollection<CryptoModel> Cryptos
+        {
+            get { return _Cryptos; }
+            set
+            {
+
+                if (_Cryptos != value)
+                {
+                    _Cryptos = value;
+                    OnNotifyPropertyChanged();
+                }
+            }
+        }
+
+    }
+}
