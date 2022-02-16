df4be7e525dc33329ff800d7d47f2d10cb795b5d
HosseinAfsharnia <h.afsharnia@gmail.com>
2022-02-16 15:39:14 +0100

    Adds the UserControl for Crypto

---
+++ ApplicationStock/ConsoleStockBruker/WPF_Application/UserControls/Controls/CryptoUC.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ProjectStockModels.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Application.UserControls.Controls
+{
+    /// <summary>
+    /// Interaction logic for CryptoUC.xaml
+    /// </summary>
+    public partial class CryptoUC : UserControl
+    {
+        private static readonly DependencyProperty CurrentCryptoProperty =
+    DependencyProperty.Register("CurrentCrypto", typeof(CryptoModel), typeof(CryptoUC));
+        public CryptoModel CurrentCrypto
+        {
+            get
+            {
+                return GetValue(CurrentCryptoProperty) as CryptoModel;
+            }
+
+            set
+            {
+                SetValue(CurrentCryptoProperty, value);
+            }
+        }
+        public CryptoUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
